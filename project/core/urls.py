2c6d9aaffb4c38b3927d26e24c1447b302ade27a
aakash-shakya <official.aakas@gmail.com>
2021-05-09 15:40:43 +0545

    pandas library used for avg sale for 2 years interval and returned dataframe to_html

--- project/core/urls.py
+++ project/core/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('',dataView,name="data view"),
     path('overall-sale/',overall_sale_by_country),
+    path('avg-sale/',avg_sale_of_each_product_for_2_years),
+
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -4,6 +4,7 @@ from .models import *
 
 import requests as req
 import json
+import pandas as pd
 
 
 def dataView(request):
@@ -33,3 +34,20 @@ def overall_sale_by_country(request):
         'result':Data.objects.filter(sale__gt=0).values('country','product').annotate(Sum('sale')).order_by()
     }
     return render(request,'overallSale.html',context)
+
+def avg_sale_of_each_product_for_2_years(request):
+    data = Data.objects.all().values('year','sale','country','product')
+    data = pd.DataFrame(list(data))
+   
+    data['year'] = pd.to_datetime(data['year'], format = '%Y')
+    final_df = data.groupby(['product', pd.Grouper(key = 'year', freq = '2Y', closed = 'left')]).agg(
+                    {'year': lambda x: '-'.join((str(min(x.dt.year)), str(max(x.dt.year)))),
+                    'sale':'mean',
+                    
+                    },data['sale']).reset_index(level= 0).reset_index(drop=True)
+    
+    html=final_df.to_html(border=1,table_id='design',col_space=125)
+    context={
+        'result':html,
+    }
+    return render(request,'avgSale.html',context)
