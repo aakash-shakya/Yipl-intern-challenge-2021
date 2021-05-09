b619fad205fe0a32c0f65f65654a172da37055b2
aakash-shakya <official.aakas@gmail.com>
2021-05-09 08:28:55 +0545

    fetching api and storing in database worked. For Development " storing data after api " everytime is commented

--- project/core/views.py
+++ project/core/views.py
@@ -9,17 +9,17 @@ def dataView(request):
     url = "https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json"
     res = req.get(url).json()
 
-    try:
-        for i in res:
-            info = Data()
-            info.year=i['year']
-            info.sale=i['sale']
-            info.product=i['petroleum_product']
-            info.country=i['country']
-            info.save()
+    # try:
+    #     for i in res:
+    #         info = Data()
+    #         info.year=i['year']
+    #         info.sale=i['sale']
+    #         info.product=i['petroleum_product']
+    #         info.country=i['country']
+    #         info.save()
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
 
     context ={
         'overall_sale_by_country':Data.objects.all(),
