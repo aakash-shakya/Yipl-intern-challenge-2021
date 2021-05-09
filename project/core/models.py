8d49261ce7eaad4ae1b79f6a67edbb4b8c55b12d
aakash-shakya <official.aakas@gmail.com>
2021-05-09 07:58:51 +0545

    models (  Table "Data" ) created.

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    class Meta:
+        ordering = ("year",)
+    year = models.IntegerField()
+    sale = models.IntegerField()
+    country = models.CharField(max_length=15,null=True)
+    product = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.product
