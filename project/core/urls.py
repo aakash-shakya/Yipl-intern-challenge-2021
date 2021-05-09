0be66fffa2c2fc2dfe28f76fdc53d789aa579472
aakash-shakya <official.aakas@gmail.com>
2021-05-09 08:06:26 +0545

    view for index ( fetching json data from api endpoint and storing )

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('',dataView,name="data view"),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+import requests as req
+import json
+
+
+def dataView(request):
+    url = "https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json"
+    res = req.get(url).json()
+
+    try:
+        for i in res:
+            info = Data()
+            info.year=i['year']
+            info.sale=i['sale']
+            info.product=i['petroleum_product']
+            info.country=i['country']
+            info.save()
+
+    except Exception as e:
+        print(e)
+
+    context ={
+        'overall_sale_by_country':Data.objects.all(),
+    }
+    return render(request, 'index.html',context)
